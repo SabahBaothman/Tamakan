d4476d52a1e605eed7fbf742bd3e5e1aeffc6601
Sabah <sadbullahbaothman@stu.kau.edu.sa>
2024-06-28 22:05:53 +0300

    add nav bar

---
+++ pages/nav.php
@@ -0,0 +1,20 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Navbar</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <!-- Navigation bar -->
+    <nav>
+        <div class="nav-left">
+            <a href="/index.php" id="nav-logo-holder">
+                <img id="logo" src="../images/logo.png" alt="Logo" width="80">
+            </a>
+        </div>
+    </nav>
+</body>
+</html>
