67ca41186aa8608a3ee3d9131e25ae9283ccef5c
Roaa Altunsi <camyror7@gmail.com>
2024-06-29 23:08:02 +0300

    Add Front-end of Chapters Page

---
+++ pages/chapters.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../style.css">
+    <title>Chapters</title>
+</head>
+
+<?php
+include('./nav.php');
+?>
+
+<body>
+    <div class="paddContainer">
+
+        <!-- HEADER -->
+        <div class="header">
+            <div class="pageTitle">
+                <h2>Chapters</h2>
+            </div>
+            <div class="pageSubtitle">
+                <p><span>Database / CPCS-241</span></p>
+                <button type="button">Add Chapter</button>
+            </div>
+        </div>
+
+        <!-- CHAPTERS -->
+        <div id="chapters">
+            <a href="" class="chapterCard">
+                <h1>CH1</h1>
+                <div>
+                    <p>Database and Database users</p>
+                </div>
+            </a>
+
+            <a href="" class="chapterCard">
+                <h1>CH1</h1>
+                <div>
+                    <p>Database and Database users</p>
+                </div>
+            </a>
+
+            <a href="" class="chapterCard">
+                <h1>CH1</h1>
+                <div>
+                    <p>Database and Database users</p>
+                </div>
+            </a>
+
+            <a href="" class="chapterCard">
+                <h1>CH1</h1>
+                <div>
+                    <p>Database and Database users</p>
+                </div>
+            </a>
+
+        </div>
+
+    </div>
+</body>
+
+</html>
